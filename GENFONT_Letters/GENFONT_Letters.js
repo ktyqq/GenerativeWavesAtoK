430cc825ae0f3a2572e50a68a3f15b7969adb243
ker <83621112+ktyqq@users.noreply.github.com>
2021-05-30 21:53:30 +0800

    Add files via upload

---
+++ GENFONT_Letters/GENFONT_Letters.js
@@ -0,0 +1,51 @@
+/*
+ * @name Letters
+ * @description Letters can be drawn to the screen by loading a font, setting
+ * its characteristics and then drawing the letters. This example uses a for
+ * loop and unicode reference numbers to automatically fill the canvas with
+ * characters in a grid. Vowels are selected and given a specific fill color.
+ */
+let font, counter, letter,cnv;
+let fontsize ;
+
+function preload() {
+  // Ensure the .ttf or .otf font stored in the assets directory
+  // is loaded before setup() and draw() are called
+  //font = loadFont('data/SourceSansPro-Regular.otf');
+  font = loadFont('data/GenerativeWaves.otf');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  fill(255);
+  counter=65;
+  letter = char(counter);
+  // Set text characteristics
+  textFont(font);
+  textSize(500);
+  textAlign(CENTER, CENTER);
+}
+
+function draw() {
+  background(0);
+    // Draw the letter to the screen
+    text(letter, windowWidth/2, windowHeight*0.4);
+  
+  push();
+    textSize(24);
+    textAlign(CENTER, CENTER);
+    fill(255);
+    textFont('Helvetica');
+    text("type, lowercase and punctuations!\n space to save, click to play with generative waves", windowWidth/2, windowHeight*0.8);
+  pop();
+
+}
+function mousePressed() {
+  window.open("https://jsfiddle.net/");
+    }
+    
+function keyTyped(){
+  if (key === ' '){
+  saveCanvas(letter, 'jpg');}
+  else {letter=key;}
+}
---
+++ generativewaves/generativewaves.js
@@ -0,0 +1,83 @@
+var t;
+var title = 'welcome to \n generative waves';
+var myText = 'click to pause/play, press r to clear and enter to save!';
+var starttext = 'hit r to start!';
+var shouldDisplay = true;
+var instruct = true;
+
+function setup() {
+  const canvas = createCanvas(windowWidth, windowHeight);
+  stroke(10, 100);
+  noFill();
+  t = 0;
+  background(255);
+  frameRate(20);
+}
+
+function preload(){
+  font1 = loadFont('data/GenerativeWaves.ttf');
+}
+
+function draw() {
+  
+if(shouldDisplay){
+  stroke(10, 100);
+  noFill();
+//curves generating
+  var x1 = width  * noise(t + 10);
+  var x2 = mouseX * noise(t + 20);
+  var x3 = width * noise(t + 30);
+  var x4 = mouseX * noise(t + 40);
+  var y1 = mouseY * noise(t + 50);
+  var y2 = height * noise(t + 60);
+  var y3 = mouseY * noise(t + 70);
+  var y4 = height * noise(t + 80);
+
+  bezier(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  t += 0.009;
+  }
+
+ if(instruct){
+  //overlay
+  rectMode(CENTER);
+  noStroke();
+  fill(11, 33, 89);
+  rect(windowWidth/2, windowHeight/2, windowWidth, windowHeight);
+  //titles
+  fill(62, 131, 181);
+  textFont(font1);
+  textSize(120);
+  textAlign(CENTER, CENTER);
+  text(title, windowWidth/2, windowHeight*0.4);
+  push();
+  //instructions
+  fill(62, 131, 181);
+  textSize(20);
+  textAlign(CENTER, CENTER);
+  textFont('Helvetica');
+  text(myText, windowWidth/2, 0.8*windowHeight);
+  fill(62, 131, 181);
+  textSize(30);
+  textAlign(CENTER, CENTER);
+  textFont('Helvetica');
+  textStyle(ITALIC);
+  text(starttext, windowWidth/2, 0.9*windowHeight);
+  pop();
+  }
+}
+  
+
+function mousePressed(){
+  shouldDisplay = !shouldDisplay;
+}
+
+function keyPressed() {
+  if (keyCode === 82) {
+  instruct = false;
+  clear();
+  }
+  if (keyCode === ENTER) {
+    save();
+  } 
+}
