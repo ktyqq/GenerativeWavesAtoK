53a0c44dbe49b88827b171c2bab0786b23f42d0b
ker <83621112+ktyqq@users.noreply.github.com>
2021-06-01 09:49:47 +0800

    Add files via upload

---
+++ GENFONT_Letters.js
@@ -0,0 +1,73 @@
+/*
+ * @name Letters
+ * @description Letters can be drawn to the screen by loading a font, setting
+ * its characteristics and then drawing the letters. This example uses a for
+ * loop and unicode reference numbers to automatically fill the canvas with
+ * characters in a grid. Vowels are selected and given a specific fill color.
+ */
+
+//P5js Code
+//Credits to Karen Ann Donnachie for this Type Sampler/GENFONT code, which has been edited and adapted by me for A to the K Studio, 2021
+
+let font, counter, letter,cnv;
+let fontsize ;
+let r, g, b;
+
+function preload() {
+  // Ensure the .ttf or .otf font stored in the assets directory
+  // is loaded before setup() and draw() are called
+  //font = loadFont('data/SourceSansPro-Regular.otf');
+  font = loadFont('data/GenerativeWaves.otf');
+}
+
+function setup() {
+  r = random(240);
+  g = random(240);
+  b = random(240);
+  
+  createCanvas(windowWidth, windowHeight);
+  //fill(r, g ,b);
+  counter=54;
+  letter = char(counter);
+  // Set text characteristics
+  textFont(font);
+  textSize(500);
+  textAlign(CENTER, CENTER);
+}
+
+function draw() {
+  background(0);
+    // Draw the letter to the screen
+    fill(r, g ,b);
+    text(letter, windowWidth/2, windowHeight*0.4);
+  
+  push();
+    textSize(24);
+    textAlign(CENTER, CENTER);
+    fill(255);
+    textFont('Helvetica');
+    text("type in lowercase and punctuations, click for more colours~", windowWidth/2, windowHeight*0.85);
+  pop();
+  push();
+    textSize(45);
+    textAlign(CENTER, CENTER);
+    fill(255);
+    text("hit space to play with generative waves!", windowWidth/2, windowHeight*0.9);
+  pop();
+
+}
+function mousePressed() {
+  r = random(240);
+  g = random(240);
+  b = random(240);
+    }
+    
+function keyTyped(){
+  if (key === ' '){
+  window.open("https://ktyqq.github.io/GenerativeWavesAtoK/generativewaves/index.html");}
+  else {letter=key;
+  r = random(240);
+  g = random(240);
+  b = random(240);
+}
+}
